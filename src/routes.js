86709664e8859f71982810efa0e755892ad2c0a2
derlansilva <joderlan.silva@gmail.com>
2020-03-09 18:02:00 -0400

    Algumas funcionalidades do projeto prontas

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,7 @@
 const express = require("express");
 const routes = express.Router();
 
-const Products = require("./controllers/ProductsControllers");
 const User = require("./controllers/UserControllers");
-
-routes.get("/" , Products.index)
-routes.get("/:id" , Products.show);
-routes.post("/create" , Products.store);
-routes.put("/:id" , Products.update);
 
 //rotas usuarios
 
